b707c47895f75e966e604637d21e23fe236b242d
zwehr <zwehrmeister@gmail.com>
2023-03-05 15:34:44 -0500

    install/configure Tailwind CSS

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Home</h1>
+      <h1 className="">Home</h1>
     </>
   )
 }
