ace2c9a6ab3be206e9b00d9d045de0c3f2c77ae3
zwehr <zwehrmeister@gmail.com>
2023-03-07 11:12:39 -0500

    add Mapbox map

--- pages/map-main.js
+++ pages/map-main.js
@@ -1,10 +1,21 @@
+import Map from "react-map-gl";
 import Navbar from "@/components/Navbar";
 
 export default function MapMain() {
+  console.log(process.env.NEXT_PUBLIC_MAP_KEY);
   return (
     <>
       <Navbar />
-      <h1 className="text-center">Map</h1>
+      <Map
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14,
+        }}
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAP_KEY}
+      />
     </>
-  )
+  );
 }
