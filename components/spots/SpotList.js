62da9d94c99f7ea5f9e63b56295c22102bed34d7
zwehr <zwehrmeister@gmail.com>
2023-03-20 14:33:01 -0400

    add shadows to navbar, spot list

--- components/spots/SpotList.js
+++ components/spots/SpotList.js
@@ -1,6 +1,6 @@
 export default function SpotList() {
   return (
-    <div className="fixed top-20 w-80 h-4/5 rounded-r-lg bg-gray-100">
+    <div className="fixed top-20 w-80 h-4/5 rounded-r-lg bg-gray-100 shadow-xl">
       List of spots currently on map goes here...
     </div>
   );
