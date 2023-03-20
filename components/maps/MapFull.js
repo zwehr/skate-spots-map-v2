5cb82822ef0f126f14fc3454094cadf7c4c7f7d1
zwehr <zwehrmeister@gmail.com>
2023-03-19 23:04:35 -0400

    hide GoogleMap full screen button, move map type buttons

--- components/maps/MapFull.js
+++ components/maps/MapFull.js
@@ -2,13 +2,21 @@ import { useMemo } from "react";
 import { GoogleMap, MarkerF } from "@react-google-maps/api";
 
 export default function MapFull() {
+  const options = {
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.RIGHT_BOTTOM,
+    },
+    fullscreenControl: false,
+  };
+
   const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
 
   return (
     <div>
       <GoogleMap
+        options={options}
         zoom={10}
-        center={{ lat: 44, lng: -80 }}
+        center={center}
         mapContainerClassName="w-full h-screen"
       >
         <MarkerF position={{ lat: 44, lng: -80 }}></MarkerF>
