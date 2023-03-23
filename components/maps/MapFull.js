a51f07e98760cb0f191013d2e72667afab68745d
zwehr <zwehrmeister@gmail.com>
2023-03-22 20:13:59 -0400

    fetch and map all spots

--- components/maps/MapFull.js
+++ components/maps/MapFull.js
@@ -1,8 +1,36 @@
-import { useMemo } from "react";
+import { useState, useCallback, useEffect, useMemo } from "react";
 import { GoogleMap, MarkerF } from "@react-google-maps/api";
 
 export default function MapFull() {
-  const options = {
+  const API_URL = "http://localhost:3000/spots";
+  const [map, setMap] = useState(null);
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    if (map) {
+      const bounds = new window.google.maps.LatLngBounds();
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchSpots = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const spots = await response.json();
+        setSpots(spots);
+        console.log(spots);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    (async () => await fetchSpots())();
+  }, []);
+
+  const onLoad = useCallback((map) => {
+    setMap(map);
+  }, []);
+
+  const UiOptions = {
     zoomControlOptions: {
       position: google.maps.ControlPosition.RIGHT_BOTTOM,
     },
@@ -18,12 +46,23 @@ export default function MapFull() {
   return (
     <div>
       <GoogleMap
-        options={options}
+        options={UiOptions}
         zoom={10}
         center={center}
         mapContainerClassName="w-full h-screen"
+        onLoad={onLoad}
+        onBoundsChanged={(event) => {
+          if (map) {
+            console.log(map.getBounds().toJSON());
+          }
+        }}
       >
-        <MarkerF position={{ lat: 44, lng: -80 }}></MarkerF>
+        {spots.map((spot) => (
+          <MarkerF
+            position={{ lat: spot.lat, lng: spot.long }}
+            onClick={() => alert(spot.name)}
+          ></MarkerF>
+        ))}
       </GoogleMap>
     </div>
   );
