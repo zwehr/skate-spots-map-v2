9857d8433a487c3fe47761f31530f338e2e50059
zwehr <zwehrmeister@gmail.com>
2023-03-25 23:03:03 -0400

    create component for displaying/deleting YouTube links on AddSpot page

---
+++ components/youtube/DeletableYoutubeLinks.js
@@ -0,0 +1,20 @@
+import { AiOutlineLink } from "react-icons/ai";
+
+export default function DeletableYoutubeLinks(props) {
+  return (
+    <ul>
+      {props.links.map((link) => (
+        <div className="flex ml-5 mb-1">
+          <AiOutlineLink className="mt-2" />
+          <li className="p-1">{link}</li>{" "}
+          <button
+            className="ml-2 px-2 py-1 rounded shadow-md bg-red-400 text-gray-900 hover:bg-red-500 hover:text-black"
+            onClick={() => props.handleDelete(link)}
+          >
+            Delete Link
+          </button>
+        </div>
+      ))}
+    </ul>
+  );
+}
--- pages/add/add-spot.js
+++ pages/add/add-spot.js
@@ -1,3 +1,4 @@
+import DeletableYoutubeLinks from "@/components/youtube/DeletableYoutubeLinks";
 import { useState } from "react";
 
 export default function AddSpot() {
@@ -9,7 +10,22 @@ export default function AddSpot() {
   const [type, setType] = useState("");
   const [status, setStatus] = useState("");
   const [tags, setTags] = useState([]);
+  const [youtubeLink, setYoutubeLink] = useState("");
   const [youtubeLinks, setYoutubeLinks] = useState([]);
+
+  const handleAddYoutubeLink = () => {
+    // If text input is empty, nothing happens
+    if (youtubeLink.length > 0) {
+      setYoutubeLinks((prevLinks) => [...prevLinks, youtubeLink]);
+      setYoutubeLink("");
+    }
+  };
+
+  const handleDeleteYoutubeLink = (link) => {
+    setYoutubeLinks((prevLinks) =>
+      prevLinks.filter((prevLink) => prevLink !== link)
+    );
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,13 +34,13 @@ export default function AddSpot() {
   return (
     <>
       <h1>Add Spot</h1>
-      <div className="p-3 mx-auto w-1/2 bg-gray-200 rounded-lg shadow-lg">
+      <div className="p-3 mx-auto mb-10 w-1/2 bg-gray-200 rounded-lg shadow-lg">
         <form onSubmit={handleSubmit}>
           <label htmlFor="name" className="block">
             Name:
             <input
               type="text"
-              className="block ml-2 p-1 focus:outline-none"
+              className="block w-1/2 ml-2 p-1 focus:outline-none"
               placeholder="Enter spot name..."
             />
           </label>
@@ -39,7 +55,7 @@ export default function AddSpot() {
             Latitude:
             <input
               type="text"
-              className="block ml-2 p-1 focus:outline-none"
+              className="block w-1/3 ml-2 p-1 focus:outline-none"
               placeholder="Enter latitude..."
             />
           </label>
@@ -47,7 +63,7 @@ export default function AddSpot() {
             Longitude:
             <input
               type="text"
-              className="block ml-2 p-1 focus:outline-none"
+              className="block w-1/3 ml-2 p-1 focus:outline-none"
               placeholder="Enter longitude..."
             />
           </label>
@@ -60,7 +76,10 @@ export default function AddSpot() {
           </label>
           <label htmlFor="type" className="block my-5">
             Type:
-            <select value={type} className="ml-2 p-1 focus:outline-none block">
+            <select
+              value={type}
+              className="ml-2 p-1 w-1/3 focus:outline-none block"
+            >
               <option value="handrail">Handrail</option>
               <option value="stairs">Stairs</option>
               <option value="gap">Gap</option>
@@ -87,7 +106,11 @@ export default function AddSpot() {
           <label htmlFor="tags" className="block my-5">
             Tags:
             <div>
-              <input type="text" className="mx-2 p-1 focus:outline-none" />
+              <input
+                type="text"
+                className="w-1/3 mx-2 p-1 focus:outline-none"
+                placeholder="Enter a tag and click 'Add Tag'..."
+              />
               <button className="py-1 px-2 bg-green-400 text-gray-900 rounded hover:bg-green-500 hover:text-black shadow-md">
                 Add Tag
               </button>
@@ -98,14 +121,33 @@ export default function AddSpot() {
             <div>
               <input
                 type="text"
+                value={youtubeLink}
+                onChange={(e) => setYoutubeLink(e.target.value)}
                 className="w-2/3 mx-2 p-1 focus:outline-none"
+                placeholder="Enter a YouTube link and click 'Add YouTube Link'..."
               />
-              <button className="py-1 px-2 bg-green-400 text-gray-900 rounded hover:bg-green-500 hover:text-black shadow-md">
+              <button
+                className="py-1 px-2 bg-green-400 text-gray-900 rounded hover:bg-green-500 hover:text-black shadow-md"
+                onClick={handleAddYoutubeLink}
+              >
                 Add YouTube Link
               </button>
             </div>
           </label>
-          <div className="w-1/12 mx-auto">
+          <div>
+            {youtubeLinks.length > 0 ? (
+              <DeletableYoutubeLinks
+                links={youtubeLinks}
+                handleDelete={handleDeleteYoutubeLink}
+              />
+            ) : (
+              <p className="ml-5 italic">
+                No YouTube links added. Add one or more, and they will appear
+                here.
+              </p>
+            )}
+          </div>
+          <div className="w-1/12 mx-auto mt-8 mb-2">
             <input
               type="submit"
               value="Add Spot"
