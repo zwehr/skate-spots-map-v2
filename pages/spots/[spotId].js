b805ac5f7e0a1ebda52dc27cbf4331b3c858de84
zwehr <zwehrmeister@gmail.com>
2023-03-29 19:20:02 -0400

    add YouTube embed, description, tags, etc to [spotId] page

--- pages/spots/[spotId].js
+++ pages/spots/[spotId].js
@@ -5,6 +5,11 @@ export default function SpotById() {
   const [spot, setSpot] = useState({
     name: "",
     description: "",
+    lat: "",
+    lng: "",
+    type: "",
+    status: "",
+    tags: [],
     youtubeLinks: [],
   });
 
@@ -32,7 +37,41 @@ export default function SpotById() {
   return (
     <div className="w-1/2 mx-auto">
       <h1>{spot.name}</h1>
-      <p className="text-center">Description, YouTube, etc. will go here</p>
+      <div className="w-[550px] my-5 mx-auto">
+        <iframe
+          /* Default width and height are 560 and 315 respectively. */
+          width="550"
+          height="315"
+          src={spot.youtubeLinks[0]}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <p className="m-3">
+        <span className="font-bold">Description:</span> {spot.description}
+      </p>
+      <p className="m-3">
+        <span className="font-bold">Type:</span>{" "}
+        {spot.type.charAt(0).toUpperCase() + spot.type.slice(1)}
+      </p>
+      <p className="m-3">
+        <span className="font-bold">Tags:</span>{" "}
+        {spot.tags.map((tag) => tag + " ")}
+      </p>
+      <p className="m-3">
+        <span className="font-bold">Status:</span>{" "}
+        {spot.status.charAt(0).toUpperCase() + spot.status.slice(1)}
+        {spot.status === "active" && (
+          <span> (not skate-stopped, demolished, etc.)</span>
+        )}
+      </p>
+      <p className="m-3">
+        <span className="font-bold">Latitude:</span> {spot.lat}
+      </p>
+      <p className="m-3">
+        <span className="font-bold">Longitude:</span> {spot.lng}
+      </p>
     </div>
   );
 }
