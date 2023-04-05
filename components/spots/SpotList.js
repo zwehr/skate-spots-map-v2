3c195b5845d821f9a1c63799f4ef43c8a3d356ef
zwehr <zwehrmeister@gmail.com>
2023-04-04 20:17:32 -0400

    add container div for SpotList h2 and p below, so that they are not hidden when scrolling

--- components/spots/SpotList.js
+++ components/spots/SpotList.js
@@ -2,18 +2,20 @@ import Spot from "./Spot";
 
 export default function SpotList(props) {
   return (
-    <div className="fixed top-20 p-2 w-auto h-4/5 rounded-r-lg bg-gray-100 shadow-xl overflow-y-scroll overflow-x-hidden">
+    <div className="fixed top-20 p-2 w-auto h-4/5 rounded-r-lg bg-gray-100 shadow-x">
       <h2 className="text-xl">Current Spots</h2>
-      <p>{props.spots.length} spots found in current area:</p>
-      {props.spots.length > 0 ? (
-        <ul>
-          {props.spots.map((spot) => (
-            <Spot spot={spot} />
-          ))}
-        </ul>
-      ) : (
-        <p>Sorry, no current spots, trying searching somewhere else...</p>
-      )}
+      <p className="mb-2">{props.spots.length} spots found in current area:</p>
+      <div className="h-5/6 overflow-y-scroll overflow-x-hidden">
+        {props.spots.length > 0 ? (
+          <ul>
+            {props.spots.map((spot) => (
+              <Spot spot={spot} />
+            ))}
+          </ul>
+        ) : (
+          <p>Sorry, no current spots, trying searching somewhere else...</p>
+        )}
+      </div>
     </div>
   );
 }
