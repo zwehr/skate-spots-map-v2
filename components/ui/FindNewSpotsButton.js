834b3c85d53111385b232393576ed890e55b5c7b
zwehr <zwehrmeister@gmail.com>
2023-04-10 12:33:54 -0400

    reposition and properly center "find new spots" button

--- components/ui/FindNewSpotsButton.js
+++ components/ui/FindNewSpotsButton.js
@@ -1,7 +1,7 @@
 export default function FindNewSpotsButton(props) {
   return (
     <button
-      className="absolute top-20 left-1/2 w-52 h-12 rounded-lg bg-gray-100 shadow-xl hover:bg-gray-50"
+      className="absolute left-0 right-0 mx-auto top-6 w-52 h-12 rounded-lg bg-gray-100 shadow-xl hover:bg-gray-50"
       onClick={props.handleClick}
     >
       Find spots in this area.
